e402d4b77ee467917f262ed76329d282af12e949
Volodymyr Novak <novakvova@gmail.com>
2021-02-01 16:52:30 +0200

    Show list users

--- src/main/java/app/web/HomeController.java
+++ src/main/java/app/web/HomeController.java
@@ -1,5 +1,9 @@
 package app.web;
 
+import app.entities.User;
+import java.util.*;
+import app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "index";
     }
 }
