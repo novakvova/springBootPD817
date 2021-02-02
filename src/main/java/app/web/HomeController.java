725acdfca11a53a4f506319812343483463f26f8
Volodymyr Novak <novakvova@gmail.com>
2021-02-02 16:53:11 +0200

    Add edit and delete

--- src/main/java/app/web/HomeController.java
+++ src/main/java/app/web/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -41,6 +42,32 @@ public class HomeController {
         }
 
         userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateUser(@PathVariable("id") Long id, User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "edit";
+        }
+        userRepository.save(user);
+        return "redirect:/";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userRepository.delete(user);
         return "redirect:/";
     }
 
